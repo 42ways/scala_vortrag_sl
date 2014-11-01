f3b4ec3fe3fbeab300e0ed4d7090db52c34e98db
Thomas Herrmann <mail@thoherr.de>
2014-11-01 12:21:32 +0100

    combined calculation for w and w_gef

--- codesamples/Vermoegensaufteilung.scala
+++ codesamples/Vermoegensaufteilung.scala
@@ -9,3 +9,13 @@ def w(n:Int):Double = if (n<=0) 0 else w(n-1) * z + b(n)
 
 def w_gef(n:Int):Double = if (n<=0) 0 else w(n) * (w_gef(n-1) + b_gef(n)) / (w(n-1) + b(n))
 
+def W(n:Int):(Double, Double) = {
+	if (n<=0) (0, 0)
+    else {
+        val (w_n_1, w_gef_n_1) = W(n-1)
+        val w_n = w_n_1 * z + b(n)
+        val w_gef_n = w_n * (w_gef_n_1 + b_gef(n)) / (w_n_1 + b(n))
+        (w_n, w_gef_n)
+    }
+}
+
