cab1aa8d03e1182650a926bd80c11d3963bee70f
Thomas Herrmann <mail@thoherr.de>
2014-11-01 12:13:47 +0100

    first simple calculation with original formula (not optimized)

--- codesamples/Vermoegensaufteilung.scala
+++ codesamples/Vermoegensaufteilung.scala
@@ -1,1 +1,11 @@
 // Beispiel: Vermoegensaufteilung
+
+val b = Array(0, 1200, 1200, 1300, 1250, 1200, 1200, 1200)
+val b_gef = Array(0, 600, 600, 720, 580, 610, 590, 600)
+
+val z = 1.0125
+
+def w(n:Int):Double = if (n<=0) 0 else w(n-1) * z + b(n)
+
+def w_gef(n:Int):Double = if (n<=0) 0 else w(n) * (w_gef(n-1) + b_gef(n)) / (w(n-1) + b(n))
+
