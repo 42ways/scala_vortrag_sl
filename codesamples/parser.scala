4402c185b53d1f64e9670bafa15b3f5d4a69573d
Thomas Herrmann <mail@thoherr.de>
2014-11-17 22:27:47 +0100

    more examples, some more slides

---
+++ codesamples/parser.scala
@@ -0,0 +1,24 @@
+// parser combinators (fro Funcional Programming in Scala)
+
+trait Parsers[ParseError, Parser[+_]] { self =>
+
+    def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+
+    def char(c: Char): Parser[Char]
+    implicit def string(s: String): Parser[String]
+    implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
+    implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[Sting] = ParserOps[f(a)]
+
+    def or[A](s1: Parser[A], s2:Parser[A]): Parser[A]
+
+    def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+
+    def many[A](p: Parser[A]): Parser[List[A]]
+
+    def map[A,B](a: Parser[A])(f: A => B): Parser[B]
+    
+    case class ParserOps[A](p: Parser[A]) {
+        def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+        def or[B>:A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
+    }
+}
