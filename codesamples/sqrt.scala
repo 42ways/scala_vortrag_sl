b7d0373c8bdd7bb0d6ee264137221b1c3a84b327
Thomas Herrmann <mail@thoherr.de>
2014-11-20 00:04:24 +0100

    make example work

--- codesamples/sqrt.scala
+++ codesamples/sqrt.scala
@@ -1,15 +1,18 @@
 // Square root (Newton's method)
 
 def sqrt(x: Double): Double = {
-    def sqrtIter(guess: Double, x: Double) =
-                if (isGoodEnough(guess, x)) guess
-                else sqrtIter(improve(guess, x), x)
-    def isGoodEnough(guess: Double, x: Double) =
-        abs(guess * guess - x) / x < 0.001
 
-    def improve(guess: Double, x: Double) =
+    def sqrtIter(guess: Double, x: Double) : Double = {
+        println(guess, x)
+        if (isGoodEnough(guess, x)) guess
+        else sqrtIter(improve(guess, x), x)
+    }
+
+    def isGoodEnough(guess: Double, x: Double) : Boolean =
+        Math.abs(guess * guess - x) / x < 0.001
+
+    def improve(guess: Double, x: Double) : Double =
         (guess + x / guess) / 2
 
-    def sqrt(x: Double) = sqrtIter(1.0, x)
+    sqrtIter(1.0, x)
 }
-    
