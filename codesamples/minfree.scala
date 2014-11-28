70843253b46f6c4ea979b4d5419114727d1166a7
Thomas Herrmann <mail@thoherr.de>
2014-11-28 14:59:39 +0100

    minfree example

--- codesamples/minfree.scala
+++ codesamples/minfree.scala
@@ -1,5 +1,12 @@
 // Inspired by "Pearls of functional algorithm design" by Richard Bird, Cambridge University Press, Chapter 1
 
-def minfree(List[Int]): Int = {
-    
+def minfree(list: List[Int]): Int = {
+    Stream.from(1).dropWhile(list contains).head
 }
+
+def l1 = List(26, 9, 22, 3, 17, 13, 21, 25, 14, 15, 20, 7, 2, 24, 12, 8, 19, 11, 27, 6, 5, 23, 18, 1, 16, 4, 10)
+minfree(l1)
+
+def l2 = l1.filterNot(_ == 12)
+
+minfree(l2)
