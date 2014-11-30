71e2ad44297704d398e8afd2468311ac68727d5e
Thomas Herrmann <mail@thoherr.de>
2014-11-30 23:55:01 +0100

    running barwert sample

--- _TODO.md
+++
@@ -1,7 +0,0 @@
-Scala-Vortrag SwissLife ToDo
-============================
-
-* Thematische Schwerpunkte festlegen
-* Agenda fuer Praesentation
-* Programmbeispiele (allgemein und mit Bezug zur LV)
-* Beispiele vorbereiten
--- codesamples/qx_curried_barwert.scala
+++ codesamples/qx_curried_barwert.scala
@@ -1,4 +1,5 @@
-import sterbetafeln
+:load sterbetafeln.scala
+:paste qx_barwert_helper.scala
 
 def klassisches_qx(tafel: String, sex: String): Int => Double = klassische_sterbetafel(tafel, sex, _)
 
@@ -11,5 +12,16 @@ def  barwert(zins: Int => Double, qx: Int => Double, spektrum: Int => Double) (v
     if ( von > bis)
         0.0;
     else 
-        spektrum(von) + (1.0 - qx(von + eintrittsAlter)) * v(zins(von)) * barwert(zins, qx, spektrum)(von + 1, bis, sex, eintrittsAlter);
+        spektrum(von) + (1.0 - qx(von + eintrittsAlter)) * v(zins(von)) * barwert(zins, qx, spektrum)(von + 1, bis, eintrittsAlter);
 }
+
+val vp = new Person("M", 175, 68, DateFactory(1990, 1, 1))
+
+val barwertBerechnung = barwert(InterrestRate.constant0175.rate, klassisches_qx("2009/2011", vp.sex), Spektrum.yearly.fn)_
+println ("Barwert klassisch  = " + barwertBerechnung(0, 12, vp.age(DateFactory(2014, 1, 1))))
+
+val barwertBerechnung = barwert(InterrestRate.constant0175.rate, unisex_qx("2009/2011"), Spektrum.yearly.fn)_
+println ("Barwert unisex  = " + barwertBerechnung(0, 12, vp.age(DateFactory(2014, 1, 1))))
+
+val barwertBerechnung = barwert(InterrestRate.constant0175.rate, bmi_qx("2009/2011", vp.sex, vp.groesse, vp.gewicht), Spektrum.yearly.fn)_
+println ("Barwert bmi  = " + barwertBerechnung(0, 12, vp.age(DateFactory(2014, 1, 1))))
